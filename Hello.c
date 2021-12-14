6f3982e06dddfeacdcdceed72002cdf43581f91f
Minemobs <issahakim94@gmail.com>
2021-12-14 19:16:41 +0100

    Added some functions. (I got a 388 segmentation fault.)

--- Hello.c
+++ Hello.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+void println(char* text);
+char* intToBoolean(int i);
 
 int main(void) {
 	printf("Hello World!\n");
-	char fileAuthor[] = "minemobs\n";
-	printf(fileAuthor);
+	char fileAuthor[] = "minemobs";
+	println(fileAuthor);
+	printf("%s %s\n", "Is Admin ?", intToBoolean(-32767));
+	return 0;
+}
+
+void println(char* text) {
+	printf("%s\n", text);
+}
+
+char* intToBoolean(int i) {
+	return i == 1 ? "true" : i != -32767 ? "false" : "Maybe";
 }
